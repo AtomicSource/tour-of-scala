54db27455284939a442f1865ae852b0d2836a29a
AtomicSource <AtomicSource@users.noreply.github.com>
2025-09-17 07:20:17 +0000

    Tense & article correction  in methods.js
    More verb tense agreement corrections, and removing use of definite article where it doesn't fit.

--- pages/scala/methods.js
+++ pages/scala/methods.js
@@ -9,14 +9,14 @@ export const date = "2020-07-17T17:00:40"
 const scastieId = "4FCkgVNaT7ucVCga5w5qIA"
 
 const mainInfoBox = <>
-  <p>A method in programming language is a bit of code that get executed when called and return a value.</p>
+  <p>A method in a programming language is a bit of code that gets executed when called and returns a value.</p>
   <p>In Scala, the boundary between Values and Methods is very thin and we are going to learn more about all of it as we go into more complex Scala Knowledge Bits.</p><p>But, if things goes as planned, today should be simple.</p>
 </>
 
 const detailedInfoBox = <>
   <p>I hope you had fun with today's Scala Knowledge Bit ! </p>
-  <p>One main thing to note, if the method do not take arguments, you do not need the <code>(...)</code>, the parentheses are optional.</p>
-  <p>In practice, you have to be careful when calling the method. Be sure to use parentheses when it was defined with them. And don't use the parentheses when the method was defined without them.</p>
+  <p>One main thing to note, if the method does not take arguments, you do not need the <code>(...)</code>, the parentheses are optional.</p>
+  <p>In practice, you have to be careful when calling a method. Be sure to use parentheses when it was defined with them. And don't use parentheses when the method was defined without them.</p>
 </>
 
 const description = "Scala Knowledge Bits - Scala Methods - Periodic exercise to learn bits of knowledge about Scala. Scala Methods here."
